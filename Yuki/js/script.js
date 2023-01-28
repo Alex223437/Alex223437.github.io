56bd50173da886c4dc724529b468c61081f15a43
Alex223437 <65247595+Alex223437@users.noreply.github.com>
2023-01-28 15:20:43 +0100

    Add files via upload

---
+++ Yuki/js/script.js
@@ -0,0 +1,27 @@
+const slider = tns({
+  container: '.carousel__slider',
+  items: 1,
+  slideBy: 'page',
+  speed: 500,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  controls: false,
+  nav: false,
+  mouseDrag: true,
+  autoplayHoverPause: true
+  });
+
+  document.querySelector('.carousel__button_prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.carousel__button_next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+
+const teamSlider = new Swiper('.team-carousel', {
+  loop: true,
+  slidesPerView: 3,
+  speed: 1000,
+  centeredSlides: true
+});
